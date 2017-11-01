db1762280296adae60d5400a1b935f402b162579
Slawomir Krupa <slawomir01krupa@gmail.com>
2017-11-01 16:45:02 +0000

    lesson about styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require("gulp");
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+
+
+
 
 gulp.task('default', function(){
 
@@ -15,7 +24,9 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
 
-	console.log("imagine sass running here");
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssImport, cssvars, nested ,autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
 
 });
 
